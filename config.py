09c4b4feff33485a72d22b7bbe53b9dedcc644df
Mich707 <00381721@uca.edu.sv>
2025-09-28 12:41:42 -0600

    fix: not using maps api key

--- config.py
+++ config.py
@@ -6,8 +6,10 @@ load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
-GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
 
-# Validación básica
-if not TELEGRAM_TOKEN or not GEMINI_API_KEY or not WEATHER_API_KEY or not GOOGLE_MAPS_API_KEY:
-    raise ValueError("❌ Falta alguna API key en .env")
+if not TELEGRAM_TOKEN:
+    raise ValueError("❌ Falta TELEGRAM_TOKEN en el archivo .env")
+if not GEMINI_API_KEY:
+    raise ValueError("❌ Falta GEMINI_API_KEY en el archivo .env")
+if not WEATHER_API_KEY:
+    raise ValueError("❌ Falta WEATHER_API_KEY en el archivo .env")
