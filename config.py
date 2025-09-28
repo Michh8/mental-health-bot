b4fae01cd1d4ecf82fd9d7b72c40e00ab333eb72
Mich707 <00381721@uca.edu.sv>
2025-09-28 12:24:28 -0600

    fix: config.py

--- config.py
+++ config.py
@@ -6,3 +6,8 @@ load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
+
+# Validación básica
+if not TELEGRAM_TOKEN or not GEMINI_API_KEY or not WEATHER_API_KEY or not GOOGLE_MAPS_API_KEY:
+    raise ValueError("❌ Falta alguna API key en .env")
